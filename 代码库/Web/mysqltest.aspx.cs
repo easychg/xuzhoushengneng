6f54695591cb774670ebee1c5b33764280e16747
easychg <156375001@qq.com>
2018-03-07 16:38:12 +0800

    dontno

---
+++ 代码库/Web/mysqltest.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Web
+{
+    public partial class mysqltest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) {
+                bindData();
+            }
+        }
+
+        private void bindData()
+        {
+            string sql = "SELECT * FROM phome_ecms_article";
+            rpt_a.DataSource = MysqlHelper.GetDataSet(MysqlHelper.Conn, CommandType.Text, sql, null).Tables[0];
+            rpt_a.DataBind();
+        }
+    }
+}
